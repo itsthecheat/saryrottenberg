0f3153ed4b185b61e31afccb21a127cf7ee31d54
Leslie Behum <llaurenb@gmail.com>
2019-09-20 18:20:48 -0400

    add smooth scroll, hero overlay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 console.log('\'Allo \'Allo!');
+
+// Page Scroll transition effect when clilcking menu item
+$('.page-scroll').click(function() {
+var sectionTo = $(this).attr('href');
+$('html, body').animate({
+ scrollTop: $(sectionTo).offset().top
+}, 800);
+});
+
+
+// $('.box').on('tap', function(e){
+//   var box = $(this);
+//   box.show('. overlay-effect');
+//   e.preventDefault();
+//   console.log('tapping the box');
+// })
+
+// document.addEventListener("touchstart", function() {}, true);
