c1083d4d86ae41900e90667e8c663fc997eca4a7
Leslie Behum <llaurenb@gmail.com>
2019-11-01 14:01:38 -0400

    fix styling footer, fix specialties menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ $('.box').on('touchstart', function() {});
 $('.page-scroll').click(function() {
 var sectionTo = $(this).attr('href');
 $('html, body').animate({
- scrollTop: $(sectionTo).offset().top
+ scrollTop: $(sectionTo).offset().top -80
 }, 800);
 });
 
@@ -16,6 +16,14 @@ const util = mdc.ripple.util;
 (document.querySelectorAll('.my-ripple')).forEach((el) => mdc.ripple.MDCRipple.attachTo(el));
 (document.querySelectorAll('button')).forEach((el) =>
 mdc.ripple.MDCRipple.attachTo(el));
+const buttonEl = document.querySelector('.mdc-button');
+const menuEl = document.querySelector('.mdc-menu');
+const menu = new mdc.menu.MDCMenu(menuEl);
+buttonEl.addEventListener('click', (event) => {
+  menu.open = !menu.open;
+  menu.setAnchorCorner(mdc.menu.Corner.BOTTOM_LEFT);
+  menu.setAnchorElement(buttonEl);
+});
 
 // new MDCRipple.attachTo(document.querySelector('.my-ripple-button'));
 // const surface = document.querySelector('.my-ripple');
